2f460bf65438613a4474a1f85df0a4d4a53f81bc
tlomba <trevorlomba@gmail.com>
2021-12-01 15:33:45 -0500

    api routes for song management and authorization added

---
+++ app/models/song.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const songSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  artist: {
+    type: String,
+    required: true
+  },
+  album: {
+    type: String,
+    required: false
+  },
+  isrc: {
+    type: Number,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Song', songSchema)
--- app/routes/song_routes.js
+++ app/routes/song_routes.js
@@ -3,8 +3,8 @@ const express = require('express')
 // Passport docs: http://www.passportjs.org/docs/
 const passport = require('passport')
 
-// pull in Mongoose model for examples
-const Example = require('../models/example')
+// pull in Mongoose model for songs
+const Song = require('../models/song')
 
 // this is a collection of methods that help us detect situations when we need
 // to throw a custom error
@@ -17,7 +17,7 @@ const handle404 = customErrors.handle404
 const requireOwnership = customErrors.requireOwnership
 
 // this is middleware that will remove blank fields from `req.body`, e.g.
-// { example: { title: '', text: 'foo' } } -> { example: { text: 'foo' } }
+// { song: { title: '', text: 'foo' } } -> { song: { text: 'foo' } }
 const removeBlanks = require('../../lib/remove_blank_fields')
 // passing this as a second argument to `router.<verb>` will make it
 // so that a token MUST be passed for that route to be available
@@ -28,38 +28,37 @@ const requireToken = passport.authenticate('bearer', { session: false })
 const router = express.Router()
 
 // INDEX
-// GET /examples
-router.get('/examples', requireToken, (req, res, next) => {
-  Example.find()
-    // respond with status 200 and JSON of the examples
-    .then(examples => res.status(200).json({ examples: examples }))
+// GET /songs
+router.get('/songs', requireToken, (req, res, next) => {
+  Song.find()
+    // respond with status 200 and JSON of the songs
+    .then(songs => res.status(200).json({ song: songs }))
     // if an error occurs, pass it to the handler
     .catch(next)
 })
 
 // SHOW
-// GET /examples/5a7db6c74d55bc51bdf39793
-router.get('/examples/:id', requireToken, (req, res, next) => {
+// GET /songs/5a7db6c74d55bc51bdf39793
+router.get('/songs/:id', requireToken, (req, res, next) => {
   // req.params.id will be set based on the `:id` in the route
-  Example.findById(req.params.id)
+  Song.findById(req.params.id)
     .then(handle404)
-    // if `findById` is succesful, respond with 200 and "example" JSON
-    .then(example => res.status(200).json({ example: example }))
+    // if `findById` is succesful, respond with 200 and "song" JSON
+    .then(song => res.status(200).json({ song: song }))
     // if an error occurs, pass it to the handler
     .catch(next)
 })
 
 // CREATE
-// POST /examples
-router.post('/examples', requireToken, (req, res, next) => {
-  console.log('true')
-  // set owner of new example to be current user
-  req.body.example.owner = req.user.id
-  console.log('true')
-  Example.create(req.body.example)
-    // respond to succesful `create` with status 201 and JSON of new "example"
-    .then(example => {
-      res.status(201).json({ example })
+// POST /songs
+router.post('/songs', requireToken, (req, res, next) => {
+  // set owner of new song to be current user
+  req.body.song.owner = req.user.id
+
+  Song.create(req.body.song)
+    // respond to succesful `create` with status 201 and JSON of new "song"
+    .then(song => {
+      res.status(201).json({ song })
     })
     // if an error occurs, pass it off to our error handler
     // the error handler needs the error message and the `res` object so that it
@@ -68,18 +67,18 @@ router.post('/examples', requireToken, (req, res, next) => {
 })
 
 // UPDATE
-// PATCH /examples/5a7db6c74d55bc51bdf39793
-router.patch('/examples/:id', requireToken, removeBlanks, (req, res, next) => {
+// PATCH /songs/5a7db6c74d55bc51bdf39793
+router.patch('/songs/:id', requireToken, removeBlanks, (req, res, next) => {
   // if the client attempts to change the `owner` property by including a new
   // owner, prevent that by deleting that key/value pair
-  delete req.body.example.owner
+  delete req.body.song.owner
 
-  Example.findById(req.params.id)
+  Song.findById(req.params.id)
     .then(handle404)
-    // ensure the signed in user (req.user.id) is the same as the example's owner (example.owner)
-    .then(example => requireOwnership(req, example))
-    // updating example object with exampleData
-    .then(example => example.updateOne(req.body.example))
+    // ensure the signed in user (req.user.id) is the same as the song's owner (song.owner)
+    .then(song => requireOwnership(req, song))
+    // updating song object with songData
+    .then(song => song.updateOne(req.body.song))
     // if that succeeded, return 204 and no JSON
     .then(() => res.sendStatus(204))
     // if an error occurs, pass it to the handler
@@ -87,14 +86,14 @@ router.patch('/examples/:id', requireToken, removeBlanks, (req, res, next) => {
 })
 
 // DESTROY
-// DELETE /examples/5a7db6c74d55bc51bdf39793
-router.delete('/examples/:id', requireToken, (req, res, next) => {
-  Example.findById(req.params.id)
+// DELETE /songs/5a7db6c74d55bc51bdf39793
+router.delete('/songs/:id', requireToken, (req, res, next) => {
+  Song.findById(req.params.id)
     .then(handle404)
-  // ensure the signed in user (req.user.id) is the same as the example's owner (example.owner)
-    .then(example => requireOwnership(req, example))
-    // delete example from mongodb
-    .then(example => example.deleteOne())
+  // ensure the signed in user (req.user.id) is the same as the song's owner (song.owner)
+    .then(song => requireOwnership(req, song))
+    // delete song from mongodb
+    .then(song => song.deleteOne())
     // send back 204 and no content if the deletion succeeded
     .then(() => res.sendStatus(204))
     // if an error occurs, pass it to the handler
